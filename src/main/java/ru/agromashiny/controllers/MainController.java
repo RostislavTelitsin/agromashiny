20a1b7379e44e55e633627577d0d6d13d846643c
Rostoslav Telitsin <rostislav.telitsin@gmail.com>
2021-05-03 09:44:40 +0700

    templates added

--- src/main/java/ru/agromashiny/controllers/MainController.java
+++ src/main/java/ru/agromashiny/controllers/MainController.java
@@ -1,12 +1,19 @@
 package ru.agromashiny.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class MainController {
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
+    @GetMapping
+    public String home(Model model) {
+        return "home";
+    }
+
+    @GetMapping("/bolt")
+    public String bolt(Model model) {
+        return "bolt";
     }
 }
