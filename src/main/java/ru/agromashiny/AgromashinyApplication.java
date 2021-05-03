279bb63fbe1603af5097aad65c95ccf260dee5b2
Rostoslav Telitsin <rostislav.telitsin@gmail.com>
2021-05-03 09:10:35 +0700

    make it better

---
+++ src/main/java/ru/agromashiny/AgromashinyApplication.java
@@ -0,0 +1,13 @@
+package ru.agromashiny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgromashinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgromashinyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/agromashiny/AgromashinyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.agromashiny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgromashinyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
