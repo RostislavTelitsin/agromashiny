a6160fb298b9ffd97e42e9f598a830c00711afde
Rostoslav Telitsin <rostislav.telitsin@gmail.com>
2022-05-10 10:45:41 +0700

    templates problem fixing

--- src/main/java/ru/agromashiny/controllers/MainController.java
+++ src/main/java/ru/agromashiny/controllers/MainController.java
@@ -2,6 +2,7 @@ package ru.agromashiny.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import ru.agromashiny.models.*;
--- src/main/java/ru/agromashiny/securingweb/WebSecurityConfig.java
+++ src/main/java/ru/agromashiny/securingweb/WebSecurityConfig.java
@@ -33,7 +33,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public void configure(WebSecurity web) throws Exception {
         web
                 .ignoring()
-                .antMatchers("/resources/**", "/static/**", "/templates/**", "/css/**", "/js/**", "/img/**");
+                /*.antMatchers("/resources/**");*/
+                .antMatchers("/resources/**", "/static/**", "/templates/**", "/css/**", "/js/**", "/img/**", "/blocks/**");
     }
 
 
